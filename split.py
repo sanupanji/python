732ce2a25be523c163c2b27e191f6bde10e26dbc
Sanu Panji <sanupanji98@gmail.com>
2019-02-23 15:09:46 +0530

    split

---
+++ split.py
@@ -0,0 +1,9 @@
+st = "Print only the swords that starts with s in this sentence"
+print(st.split(" "))
+for word in st.split(" "):
+    if word[0] == "s" or word[0] == "S":
+        print(word, end=" ")
+
+
+sw = [x for x in st.split(" ") if x[0] == "s"]
+print(sw)
